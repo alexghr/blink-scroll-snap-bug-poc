ca0d7b2ec6a09a50a3d4fe3d60f2726deffb6472
Alex Gherghisan <alexghr@users.noreply.github.com>
2022-05-30 19:58:45 +0100

    feat: add intersection observer
    this is going to be bugged in chrome

--- script.js
+++ script.js
@@ -14,5 +14,21 @@
     }
   }
 
+  function run() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const { intersectionRatio, target } = entry;
+        target.style.opacity = String(Math.max(intersectionRatio, 0.5));
+        target.style.transform = `translateY(${(1 - intersectionRatio) * 50}px)`
+      });
+    }, {
+      root: viewport,
+      threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    });
+
+    document.querySelectorAll(".item").forEach(item => observer.observe(item));
+  }
+
   populateContainer(10);
+  run();
 })();
